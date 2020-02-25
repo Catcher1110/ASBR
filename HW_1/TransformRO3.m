bdec7e109063a23e9dbef8be1d5390336ba98cf6
root <root@LAPTOP-JC.localdomain>
2020-02-24 18:31:40 -0600

    Modified function

--- HW_1/TransformRO3.m
+++ HW_1/TransformRO3.m
@@ -19,35 +19,9 @@ end
 theta = acos(0.5*(trace(matR)-1));
 if theta == 0 % arbitrary axis is ok, we pick [1, 0, 0] here
     AA = [1, 0, 0, theta];
-elseif theta == pi %
-    xx = (matR(1,1)+1)/2;
-    yy = (matR(2,2)+1)/2;
-    zz = (matR(3,3)+1)/2;
-    xy = (matR(1,2)+matR(2,1))/4;
-    xz = (matR(1,3)+matR(3,1))/4;
-    yz = (matR(2,3)+matR(3,2))/4;
-    if matR(1,1)>matR(2,2) && matR(1,1)>matR(3,3)
-        if matR(1,1) == -1
-            AA = [0, sqrt(2)/2, sqrt(2)/2, theta]; 
-        else
-            x = sqrt(xx);
-            AA = [x, xy/x, xz/x, theta];
-        end
-    elseif matR(2,2)>matR(3,3)
-        if matR(2,2) == -1
-            AA = [sqrt(2)/2, 0, sqrt(2)/2, theta];
-        else
-            y = sqrt(yy);
-            AA = [xy/y, y, yz/y, theta];
-        end
-    else
-        if matR(3,3) == -1
-            AA = [sqrt(2)/2, sqrt(2)/2, 0, theta];
-        else
-            z = sqrt(zz);
-            AA = [xz/z, yz/z, z, theta];
-        end
-    end
+elseif theta == pi % Rotation for 180 degrees
+    S = 1/(2*(1+matR(3,3)))^(1/2);
+    AA = [matR(1,3)*S, matR(2,3)*S, (1+matR(3,3))*S, theta];
 else % No singularity happens
     w_hat = (matR - matR')/(2*sin(theta));
     AA = [w_hat(3,2), w_hat(1,3), w_hat(2,1), theta];
